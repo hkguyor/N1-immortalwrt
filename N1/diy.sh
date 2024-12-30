0bb0495fc6c465fe82eff424750ce5f009db0886
hkguyor <41732955+hkguyor@users.noreply.github.com>
2024-12-30 21:15:59 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+uci del dhcp.lan.ra
+uci del dhcp.lan.ra_slaac
+uci del dhcp.lan.ra_flags
+uci del dhcp.lan.max_preferred_lifetime
+uci del dhcp.lan.max_valid_lifetime
+uci set dhcp.lan.ignore='1'
+uci add_list network.lan.dns='192.168.50.1'
+
 # Default IP
-sed -i 's/192.168.1.1/192.168.50.99/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.50.99/g' package/base-files/files/bin/config_generate
 
 # Add packages
 git clone https://github.com/ophub/luci-app-amlogic --depth=1 package/amlogic
