2d71cb3549ca1629ce42d246324dd2150595da53
hkguyor <41732955+hkguyor@users.noreply.github.com>
2025-01-01 11:38:21 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Default IP
-sed -i 's/192.168.1.1/192.168.50.99/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.50.99/g' package/base-files/files/bin/config_generate
 
 # Add packages
 git clone https://github.com/ophub/luci-app-amlogic --depth=1 package/amlogic
