b3a1bac520ee07867e4d96d0192f953c0bfbfc52
hkguyor <41732955+hkguyor@users.noreply.github.com>
2025-01-19 20:49:01 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -11,9 +11,3 @@ cp -fr package/amlogic/luci-app-amlogic feeds/luci/applications/
 
 # Clean packages
 rm -rf package/amlogic
-
-echo "src/gz openwrt_kiddin9 https://dl.openwrt.ai/latest/packages/aarch64_cortex-a53/kiddin9" >> package/system/opkg/files/customfeeds.conf
-
-sed -i "s/DISTRIB_DESCRIPTION='[^']*'/DISTRIB_DESCRIPTION='ImmortalWrt 23.05.4 Compiled by hkguyor'/" package/base-files/files/etc/openwrt_release
-
-echo -e "\e[32m$0 [DONE]\e[0m"
