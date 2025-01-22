ba7d245150f7140b383f16f5f9dc7edddf651520
hkguyor <41732955+hkguyor@users.noreply.github.com>
2025-01-22 18:08:43 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -3,6 +3,8 @@
 # Default IP
 # sed -i 's/192.168.1.1/192.168.50.99/g' package/base-files/files/bin/config_generate
 
+sed -i 's/DISTRIB_DESCRIPTION=OpenWrt /DISTRIB_DESCRIPTION=ImmortalWrt 23.05.4 Compiled by hkguyor /g' package/base-files/files/etc/openwrt_release
+
 # Add packages
 git clone https://github.com/ophub/luci-app-amlogic --depth=1 package/amlogic
 
@@ -11,7 +13,5 @@ cp -fr package/amlogic/luci-app-amlogic feeds/luci/applications/
 
 # Clean packages
 rm -rf package/amlogic
-
-sed -i "s/DISTRIB_DESCRIPTION='[^']*'/DISTRIB_DESCRIPTION='ImmortalWrt 23.05.4 Compiled by hkguyor'/" package/base-files/files/etc/openwrt_release
 
 echo -e "\e[32m$0 [DONE]\e[0m"
